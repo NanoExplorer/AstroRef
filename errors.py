7226a6cfe348cf084220c8f68d5097bfc60ead9f
Christopher Rooney <christopher@rooneyworks.com>
2023-02-11 16:29:45 -0500

    Unauthorized error added

--- errors.py
+++ errors.py
@@ -1,10 +1,18 @@
 class InternalServerError(Exception):
     def __init__(self):
         super(InternalServerError,self).__init__()
+
+
 class APILimitError(Exception):
     def __init__(self):
         super(APILimitError,self).__init__()
+
+
 class ADSMalfunctionError(Exception):
     def __init__(self):
         super(ADSMalfunctionError,self).__init__()
         
+
+class UnauthorizedError(Exception):
+    def __init__(self):
+        super(UnauthorizedError,self).__init__()
